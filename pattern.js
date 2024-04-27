c56229b6356d4755b8d14b9767d7e32527abd950
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-27 19:01:20 +0200

    Algo

--- pattern.js
+++ pattern.js
@@ -19,6 +19,27 @@ for(let i = 1 ; i <= 5 ; i++){
     for(let j = 5; j >= i; j--){
         row += "*";
     }
-    console.log(row)
+    // console.log(row)
 }
+const rows = 5;
+const treeWidth = 11;
+
+for (let i = 1; i <= rows; i++) {
+  let row = '';
+  
+  // Ajouter des espaces avant les étoiles
+  for (let j = 1; j <= (treeWidth - (2 * i - 1)) / 2; j++) {
+    row += ' ';
+  }
+  
+  // Ajouter les étoiles
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    row += '*';
+  }
+  
+  console.log(row);
+}
+
+
+
 
