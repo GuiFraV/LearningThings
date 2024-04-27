b3ed08ce557e4b96483bd5a0b4a4b3a9ac916f3c
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-27 18:44:31 +0200

    Algo

--- pattern.js
+++ pattern.js
@@ -3,5 +3,22 @@ for(let i = 1 ; i <= 5 ; i++){
     for(let j = 1; j <= 5; j++){
         row += "*";
     }
+    // console.log(row)
+}
+
+for(let i = 1 ; i <= 5 ; i++){
+    let row = "";
+    for(let j = 1; j <= i; j++){
+        row += "*";
+    }
+    // console.log(row)
+}
+
+for(let i = 1 ; i <= 5 ; i++){
+    let row = "";
+    for(let j = 5; j >= i; j--){
+        row += "*";
+    }
     console.log(row)
 }
+
