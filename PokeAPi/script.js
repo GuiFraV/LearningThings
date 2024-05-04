b7166a5f3e16ecb3b31e98f1067b17794814fa72
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-04 19:15:08 +0200

    Add filter for fun

--- PokeAPi/script.js
+++ PokeAPi/script.js
@@ -34,3 +34,27 @@ const displayPokemon = (data) => {
         }
     });
 }
+
+
+const searchPokemon = async() => {
+
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch ressource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block"
+    }catch(error){
+        console.error(error);
+    }
+
+
+}
