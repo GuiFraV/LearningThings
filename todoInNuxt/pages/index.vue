a2e5f24ce40a59064685233a986fc853f1c79583
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-07 15:28:21 +0200

    add todo in nuxt

---
+++ todoInNuxt/pages/index.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+    const todo = ref([]);
+    const newTodo = ref("");
+    
+    const addTodo = () => {
+        if(newTodo.value !== ""){
+            todo.value.push(newTodo.value)
+            newTodo.value = "";
+        }
+    }
+
+    const removeTodo = (index) => {
+        todo.value.splice(index, 1);
+    }
+</script>
+
+
+
+<template>
+  <div>
+   <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+
+    <h1>My Todo</h1>
+    <form @submit.prevent="addTodo">
+        <input type="text" v-model="newTodo" placeholder="Write a todo">
+        <button type="submit"> Add</button>
+    </form>
+
+    <ul>
+        <li v-for="(todo, index) in todo" :key="index">
+            {{todo}}
+            <button @click="removeTodo(index)">Delete</button>
+        </li>
+    </ul> 
+
+
+  </div>
+</template>
