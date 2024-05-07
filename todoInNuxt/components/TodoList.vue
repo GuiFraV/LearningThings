23a649ce39be832c073bb1c68a96133669f348fa
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-07 16:09:32 +0200

    add todo in nuxt

---
+++ todoInNuxt/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>My Todo</h1>
+    <form @submit.prevent="addTodo">
+        <input type="text" v-model="newTodo" placeholder="Write a todo">
+        <button type="submit"> Add</button>
+    </form>
+
+    <ul>
+        <slot></slot>
+    </ul> 
+  </div>
+</template>
+
+<script setup>
+    const newTodo = ref("");
+    
+    const addTodo = () => {
+        if(newTodo.value !== "" && !props.todo.includes(newTodo.value)){
+            props.todo.push(newTodo.value);
+            newTodo.value = "";
+        }
+    }
+
+    const props = defineProps({
+        todo: Array,
+        removeTodo: Function
+    });
+</script>
--- todoInNuxt/pages/index.vue
+++ todoInNuxt/pages/index.vue
@@ -1,22 +1,3 @@
-<script setup>
-
-    const todo = ref([]);
-    const newTodo = ref("");
-    
-    const addTodo = () => {
-        if(newTodo.value !== ""){
-            todo.value.push(newTodo.value)
-            newTodo.value = "";
-        }
-    }
-
-    const removeTodo = (index) => {
-        todo.value.splice(index, 1);
-    }
-</script>
-
-
-
 <template>
   <div>
    <h1>Welcome to the homepage</h1>
@@ -24,19 +5,24 @@
       This is an auto-imported component.
     </AppAlert>
 
-    <h1>My Todo</h1>
-    <form @submit.prevent="addTodo">
-        <input type="text" v-model="newTodo" placeholder="Write a todo">
-        <button type="submit"> Add</button>
-    </form>
-
-    <ul>
-        <li v-for="(todo, index) in todo" :key="index">
-            {{todo}}
-            <button @click="removeTodo(index)">Delete</button>
-        </li>
-    </ul> 
-
-
+    <TodoList :todo="todo" :removeTodo="removeTodo">
+        <template v-slot:default>
+            <li v-for="todoItem in todo" :key="todoItem">
+                {{todoItem}}
+                <button @click="removeTodo(todoItem)">Delete</button>
+            </li>
+        </template>
+    </TodoList>
   </div>
 </template>
+
+<script setup>
+    const todo = ref([]);
+    
+    const removeTodo = (todoItem) => {
+        const index = todo.value.indexOf(todoItem);
+        if (index !== -1) {
+            todo.value.splice(index, 1);
+        }
+    }
+</script>
