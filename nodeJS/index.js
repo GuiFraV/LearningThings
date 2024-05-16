d7e704a9a52e65e73ceadf583f5623ad753cb26a
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-16 16:12:41 +0200

    feat: Add API endpoints for retrieving and updating products

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -9,12 +9,50 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
 });
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
     res.status(500).send({ message: error.message });
+  }
+});
+
+// update a product
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
